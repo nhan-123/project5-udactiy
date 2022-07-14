09400cda8331aa7796c98aa5ad62a242ca091a15
nhan-123 <letranhuunhan@gmail.com>
2022-07-14 05:45:08 -0400

    upload files

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
  
 app = Flask(__name__)
  
-@app.route('/hello')
+@app.route("/hello", methods=["GET"])
 def hello():
     return 'Hello World. My name is Le Tran Huu Nhan'
  
-app.run(host='localhost', port=5000)
+app.run(host="0.0.0.0", port=80, debug=True)
