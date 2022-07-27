6f036bdb107681e85242239b665602289fd0e863
nhan <letranhuunhan@gmail.com>
2022-07-27 17:26:26 +0700

    upload files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
  
 app = Flask(__name__)
  
-@app.route("/hello", methods=["GET"])
+@app.route("/hello", methods=["GET"]) .
 def hello():
     return 'Hello World. My name is Le Tran Huu Nhan'
  
